f829ee907bbae4793b9688e80acd05af665376dc
Noah Schefer <noahschefer@Noahs-MacBook-Pro.local>
2020-01-23 15:55:48 -0500

    Lesson 35 Complete

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -4,17 +4,20 @@
         id="1"
         thumbnail="https://bentrepreneur.biz/wp-content/uploads/2019/02/tech-1000x624.jpg"
         title="Hello There!"
-        previewText="This is my first post!" />
+        previewText="This is my first post!"
+        :isAdmin="isAdmin" />
       <PostPreview
         id="2"
         thumbnail="https://bentrepreneur.biz/wp-content/uploads/2019/02/tech-1000x624.jpg"
         title="Hello There from the Future!"
-        previewText="This is my second post!" />
+        previewText="This is my second post!"
+        :isAdmin="isAdmin" />
       <PostPreview
         id="1"
         thumbnail="https://bentrepreneur.biz/wp-content/uploads/2019/02/tech-1000x624.jpg"
         title="Hello There from the Past !"
-        previewText="This is my third post!" />
+        previewText="This is my third post!"
+        :isAdmin="isAdmin" />
     </section>
 </template>
 
@@ -24,6 +27,12 @@ import PostPreview from '~/components/Posts/PostPreview';
 export default {
   components: {
     PostPreview,
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
--- components/Posts/PostPreview.vue
+++ components/Posts/PostPreview.vue
@@ -1,14 +1,14 @@
 <template>
-  <nuxt-link class="post-preview" :to="'/posts/' + id">
-        <article>
-          <div class="post-thumbnail"
-          :style="{backgroundImage: 'url(' + thumbnail + ')'}"></div>
-          <div class="post-content">
-            <h1>{{title}}</h1>
-            <p>{{previewText}}</p>
-          </div>
-        </article>
-      </nuxt-link>
+  <nuxt-link :to="postLink" class="post-preview" >
+    <article>
+      <div class="post-thumbnail"
+      :style="{backgroundImage: 'url(' + thumbnail + ')'}"></div>
+      <div class="post-content">
+        <h1>{{title}}</h1>
+        <p>{{previewText}}</p>
+      </div>
+    </article>
+  </nuxt-link>
 </template>
 
 <script>
@@ -17,22 +17,31 @@ export default {
   props: {
     id: {
       type: String,
-      required: true
+      required: true,
     },
     title: {
       type: String,
-      required: true
+      required: true,
     },
     previewText: {
       type: String,
-      required: true
+      required: true,
     },
     thumbnail: {
       type: String,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    postLink() {
+      return this.isAdmin ? '/admin/' + this.id : '/posts/' + this.id;
+    },
+  },
+};
 </script>
 
 <style scoped>
