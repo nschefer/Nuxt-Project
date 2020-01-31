839b716a91e048f05404529ce6e8f3df477a0f0c
Noah Schefer <nbschefer@gmail.com>
2020-01-31 15:19:39 -0500

    Lesson 49 Complete

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,38 +11,10 @@ export default {
   components: {
     PostList,
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: '1',
-              title: 'First Post',
-              previewText: 'This is our first post',
-              thumbnail:
-                'https://www-tc.pbs.org/wgbh/nova/media/images/nova-wonders-can-we-build-a-brain-hero_xn7Rr8X.width-800.jpg',
-            },
-            {
-              id: '2',
-              title: 'Second Post',
-              previewText: 'This is our second post',
-              thumbnail:
-                'https://www-tc.pbs.org/wgbh/nova/media/images/nova-wonders-can-we-build-a-brain-hero_xn7Rr8X.width-800.jpg',
-            },
-          ],
-        });
-      }, 1500);
-    })
-      .then(data => {
-        return data;
-      })
-      .catch(e => {
-        context.error(e);
-      });
-  },
-  created() {
-    this.$store.dispatch('setPosts', this.loadedPosts);
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    },
   },
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -11,6 +11,29 @@ const createStore = () => {
       }
     },
     actions: {
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit('setPosts', [
+              {
+                id: '1',
+                title: 'First Post',
+                previewText: 'This is our first post',
+                thumbnail:
+                  'https://www-tc.pbs.org/wgbh/nova/media/images/nova-wonders-can-we-build-a-brain-hero_xn7Rr8X.width-800.jpg',
+              },
+              {
+                id: '2',
+                title: 'Second Post',
+                previewText: 'This is our second post',
+                thumbnail:
+                  'https://www-tc.pbs.org/wgbh/nova/media/images/nova-wonders-can-we-build-a-brain-hero_xn7Rr8X.width-800.jpg',
+              },
+            ])
+            resolve();
+          }, 1500);
+        })
+      },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)
       }
